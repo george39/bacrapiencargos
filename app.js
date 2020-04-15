4ffe6ab15471feba2f115fc83300edf4f492fd09
george39 <george35@utp.edu.co>
2020-04-16 00:54:23 +0100

    Configuaracion del cors

--- app.js
+++ app.js
@@ -7,8 +7,17 @@ var bodyParser = require('body-parser');
 var app = express();
 
 
+// cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
+
 // body parser
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 //Importar rutas
@@ -25,7 +34,7 @@ var uploadRoute = require('./routes/upload');
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/tienda', (err, res) => {
-    if( err ) throw err;
+    if (err) throw err;
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 });
 
